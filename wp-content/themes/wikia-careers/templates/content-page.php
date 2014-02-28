1fd9b29270b2246d8c79f8458ec601c9ef73aa3b
Kamil Koterba <kamil@wikia-inc.com>
2014-02-28 12:20:11 +0100

    Removing main widget area for page template

--- wp-content/themes/wikia-careers/templates/content-page.php
+++ wp-content/themes/wikia-careers/templates/content-page.php
@@ -1,4 +1,3 @@
-<?php if ( dynamic_sidebar('home_right_1') ) : else : endif; ?>
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
