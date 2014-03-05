5afaeeb83e1424ecc0aef356b08b40155ef931b0
Kamil Koterba <kamil@wikia-inc.com>
2014-03-05 15:14:22 +0100

    Adding support for contact data in footer - requires Advanced Custom Fields plugin and defined group for footer_data template with specific fields names

--- wp-content/themes/wikia-careers/templates/footer.php
+++ wp-content/themes/wikia-careers/templates/footer.php
@@ -1,6 +1,21 @@
 <footer class="content-info container" role="contentinfo">
   <div class="row">
     <div class="col-lg-12">
+		<?php
+		$pages = get_pages(array(
+		'meta_key' => '_wp_page_template',
+		'meta_value' => 'templates/footer-data.php',
+		'number' => 1
+		));
+		foreach( $pages as $page ) {
+			echo $page->post_title.'<br />';
+			the_field('email_address', $page->ID);
+			the_field('post_address', $page->ID);
+			the_field('telephone_number', $page->ID);
+			the_field('facebook_link', $page->ID);
+			the_field('github_link', $page->ID);
+		}
+		?>
       <?php dynamic_sidebar('sidebar-footer'); ?>
       <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
     </div>
