8ed19d9a2e07cf3b6d72c6cccf1b9c7910933101
Kamil Koterba <kamil@wikia-inc.com>
2014-03-27 14:35:48 +0100

    Enabling .spaced-container to be used with fixed container

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fa9df916e831bc557be60cf883e08283');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7cbdc4dd6e556f8628c083b66eee6aca');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
