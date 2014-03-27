d149db36e9ef5aa80bebeb4f4b49c19a3aa94c59
Kamil Koterba <kamil@wikia-inc.com>
2014-03-27 12:03:59 +0100

    updating main min and sctipts with previous changes in less files

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e6a2e19b789cb473a037460b5a5e8ac8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fa9df916e831bc557be60cf883e08283');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
