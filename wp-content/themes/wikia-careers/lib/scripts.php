4808c2ce5dda074d59d5847383e66b3a5ce37f2c
Kamil Koterba <kamil.koterba@gmail.com>
2014-04-09 11:27:45 +0200

    Main menu minor color enhancements

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7044c29f461558450681f684d27e6368');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0552dbd2b1725f9cc1b3a264118dc2a2');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
