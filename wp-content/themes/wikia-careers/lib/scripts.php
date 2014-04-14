7e4f799bef68e769b51a79da7ec208d28df3bd88
Kamil Koterba <kamil.koterba@gmail.com>
2014-04-14 13:13:48 +0200

    Main page sections: News, help contact, footer

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ed75f66557a93aeda81e44adabc22817');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '11129aa53a8a9ad97f94125e50b112fa');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
