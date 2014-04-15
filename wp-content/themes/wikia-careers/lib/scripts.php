78d9033e5d328bfca2a34f460ed99e913431bd3a
Kamil Koterba <kamil.koterba@gmail.com>
2014-04-15 14:31:24 +0200

    Reduce fixed container width to respect margins from spaced-container

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '11f7f2a348d4783abb904e3376133fc7');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '09d1c7d8f36eda30798b68c3836eeb48');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
