81588fa33381ac4356083c386ea15f9265706d0c
Kamil Koterba <kamil.koterba@gmail.com>
2014-04-17 11:22:20 +0200

    Header on mainpage 100% height

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9fdaa55d89eae65cb3175b5446d6388f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e6f9341583c34f66a4656ef6043e1e3a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
