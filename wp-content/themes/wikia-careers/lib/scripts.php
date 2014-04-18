d530228cf63d6448cfb02058e1feb0b7c4b4108c
Kamil Koterba <kamil.koterba@gmail.com>
2014-04-18 13:27:57 +0200

    Small fix for active link in mobile menu

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fc86bbca4eab75e41ace226ac2c1c767');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2c3cf8d49e4e01edb83d1b2495314f96');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
