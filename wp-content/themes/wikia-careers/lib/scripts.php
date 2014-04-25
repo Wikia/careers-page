8a7d6c21c76370bcfc7824b7ef6f38277e721c27
Kamil Koterba <kamil.koterba@gmail.com>
2014-04-25 12:41:30 +0200

    Job offers - modal for offer description

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3c29b58273808bf563a0c15b61a1e634');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cda3eb7da8d4376a8d127d604837ff21');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
@@ -27,7 +27,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', array(), null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), 'b7a7ab0c474658be5dfd640980523eee', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), 'a836cfb17c8d7adefe86014e7319fff9', true);
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
--- wp-content/themes/wikia-careers/templates/parent-page.php
+++ wp-content/themes/wikia-careers/templates/parent-page.php
@@ -28,19 +28,36 @@ Template Name: Parent page template
 
 					$page_link = get_page_link( $page->ID );
 					$post_title = $page->post_title;
+					$content = apply_filters('the_content', $page->post_content);
 
 					$HTML =<<<HTML
-					<div class="offer-item">
-						<!--<h2><a href="$page_link">$post_title</a></h2>-->
-						<!--<span class="cross">+</span>-->
+					<div class="offer-item" data-target="#jobModal{$page->ID}" data-toggle="modal">
 						<h2>$post_title</h2>
 						<p class="short-descr">Lokalizacja lub nazwa zespołu</p>
+					</div>
+
+					<!-- Modal -->
+					<div class="modal job-offer-modal" id="jobModal{$page->ID}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+															<button type="button" class="go-back" data-dismiss="modal" aria-hidden="true">Powrót</button>
+								<div class="modal-header">
+									<button class="btn-wikia btn-arrow alignright"><span>Aplikuj</span></button>
+									<h2 class="modal-title" id="myModalLabel">$post_title</h2>
+									<p class="short-descr">Lokalizacja lub nazwa zespołu</p>
+								</div>
+								<div class="modal-body">
+									$content
+								</div>
+							</div>
+						</div>
 					</div>
 HTML;
 
 					echo $HTML;
 				}
 				?>
+
 
 			</div>
 		</div>
