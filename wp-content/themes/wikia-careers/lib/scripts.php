06e1ec0e5411256f4eda5bb9514898425f7017c3
Kamil Koterba <kamil.koterba@gmail.com>
2014-04-28 15:44:16 +0200

    Changing Open Sans to Helvetica/Arial

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '52a678012d5b492c33ca62aec7a2042f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8b4b197fc59777ef8126c38c67debd9f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
