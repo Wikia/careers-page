3c7cb91ee6c3b08e6093bf3caefa9a501567a668
Kamil Koterba <kamil.koterba@gmail.com>
2014-05-19 12:34:42 +0200

    Adjusting styles for 7form quickform

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'caa3d128c8f3bbb5705004768ecde90c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dea45bedc0d12316794deb786e567097');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
