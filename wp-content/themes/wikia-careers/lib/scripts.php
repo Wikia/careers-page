624a4f5e479af6f6d2b5bd59a0f3b99f919221a2
Kamil Koterba <kamil.koterba@gmail.com>
2014-05-20 10:26:31 +0200

    Simplifying job offers section layout

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd0197e0e5ebdf1e25926d2f5a3bf3c68');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '445ceb99aaa36671dadc926e8fe8aea0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
