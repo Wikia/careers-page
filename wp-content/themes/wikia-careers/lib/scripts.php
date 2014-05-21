2afc121910f9aa2dd3d30859bb15e12344f25937
Kamil Koterba <kamil.koterba@gmail.com>
2014-05-22 00:36:43 +0200

    Career paths on job offers list page

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '06fb70dfe84c08cac4e86977e621a81d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1b71441465357f45892b595f386658f8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- wp-content/themes/wikia-careers/templates/parent-page.php
+++ wp-content/themes/wikia-careers/templates/parent-page.php
@@ -52,16 +52,13 @@ HTML;
 
 <section class="career-paths-short container spaced-container">
 	<div class="row">
-		<div class="col-xs-4 col-sm-6 col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1">
-			<div class="avatar">
-				<img src="<?php echo esc_url(get_stylesheet_directory_uri().'/assets/img/'); ?>nandy-avatar.jpg" alt="" class="img-circle " />
-			</div>
-			<div class="description">
-				<h2><strong>nAndy</strong> - pierwszoligowy gracz</h2>
-				<p>Lus aspedi volo qui cullanis dolo tenim rest liquodicia nonsernam veleseque
-					soluptaque volupta dus doluptas debit, autescientur asitisq uatibus dandelit.</p>
-				<a href="" class="btn-wikia-regular btn-arrow">Zobacz jego ścieżkę</a>
-			</div>
+		<div class="col-xxs-4 col-xs-4 col-sm-6 col-md-5 col-lg-4 avatar">
+			<img src="<?php echo esc_url(get_stylesheet_directory_uri().'/assets/img/'); ?>jakubo.jpg" alt="jakubo" class="person-picture" />
+		</div>
+		<div class="col-xxs-4 col-xs-4 col-sm-6 col-md-7 col-lg-7 description">
+			<h2><strong>Jakub</strong> - pierwszoligowy gracz</h2>
+			<p>Dzisiaj jest naszym najlepszym ekspertem w dziedzinie technologii mobilnych.</p>
+			<a href="" class="btn-wikia-regular btn-arrow">Zobacz jego ścieżkę</a>
 		</div>
 	</div>
 </section>
