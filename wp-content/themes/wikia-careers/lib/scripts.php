684498c4b3ae53c41cd8a290420b6e4028538877
Kamil Koterba <kamil.koterba@gmail.com>
2014-06-05 12:32:25 +0200

    Changing all 0px to 0

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'acc9a4680b6dd658c8c6a62a6a3c2dc5');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4f3783100b8563d1c9e4fd97fb58c91f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
