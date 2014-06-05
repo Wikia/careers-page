24b2041e8ff3de1d3793b1d45724c43e6d06b9d4
Kamil Koterba <kamil.koterba@gmail.com>
2014-06-05 06:50:51 +0200

    Minor - moving file name into esc_url func

--- wp-content/themes/wikia-careers/templates/footer.php
+++ wp-content/themes/wikia-careers/templates/footer.php
@@ -5,12 +5,12 @@
 
 			<div class="row">
 				<div class="col-xxs-4 col-xs-4 col-sm-3 col-md-6 col-lg-6 news-item-container">
-					<img src="<?php echo esc_url(get_stylesheet_directory_uri().'/assets/img/'); ?>news1.jpg" alt="News 1" />
+					<img src="<?php echo esc_url(get_stylesheet_directory_uri().'/assets/img/news1.jpg'); ?>" alt="News 1" />
 					<h3>I’m a good eye catchy title for news and articles and other interesting stuff.</h3>
 					<span class="post-date">Posted: <span class="date">9 March 2014</span></span>
 				</div>
 				<div class="col-xxs-4 col-xs-4 col-sm-3 col-md-6 col-lg-6 news-item-container">
-					<img src="<?php echo esc_url(get_stylesheet_directory_uri().'/assets/img/'); ?>news2.jpg" alt="News 2" />
+					<img src="<?php echo esc_url(get_stylesheet_directory_uri().'/assets/img/news2.jpg'); ?>" alt="News 2" />
 					<h3>I’m a good eye catchy title for news and articles and other interesting stuff.</h3>
 					<span class="post-date">Posted: <span class="date">9 March 2014</span></span>
 				</div>
