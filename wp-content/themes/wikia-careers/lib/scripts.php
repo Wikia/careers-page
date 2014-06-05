e786f6b3ec69129dcae6eac58f2ba445ce31f062
Kamil Koterba <kamil.koterba@gmail.com>
2014-06-05 12:31:47 +0200

    Merging button definitions

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '350b348c52426e5f15a5960d7ed10653');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'acc9a4680b6dd658c8c6a62a6a3c2dc5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
