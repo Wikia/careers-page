9a36bba30c64841a125bf96651ff15766b434df5
Kamil Koterba <kamil.koterba@gmail.com>
2014-06-06 12:38:14 +0200

    Moving prefixed css values to mixins

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '245ba60c373c6fb04a3c079b160eac6f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9cd799f3a8cafa214abae9b755f53dbb');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
