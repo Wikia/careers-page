a9457f821349eca2cda5e9775a6cbece35b53a39
Kamil Koterba <kamil.koterba@gmail.com>
2014-06-12 14:58:29 +0200

    Removing unused css selectors to reduce it's size

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9cd799f3a8cafa214abae9b755f53dbb');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e9a93875672aee7ae39b6f28abaecba5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
