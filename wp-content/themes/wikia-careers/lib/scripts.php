aaa41f8b1360886ae5a6dfc7fc8a55a5b80a62c3
Kamil Koterba <kamil.koterba@gmail.com>
2014-06-21 03:23:49 +0200

    Responsive typography for blockquote and subtext

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '17a338847731a4c7a573c4f192c7fb3a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a21a51ae2e3fc8b0559a19c18cdef5e0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
