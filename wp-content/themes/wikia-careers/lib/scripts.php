5d01b8c67ede7fca2d70d62c100e8af85d12d106
Kamil Koterba <kamil.koterba@gmail.com>
2014-06-26 06:36:31 +0200

    Splitting app.less into smaller meaningful files

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '22d0ec90967090bee28f815bef3b69e0');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e53be258dda100fd1341e96f1bb8f226');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
