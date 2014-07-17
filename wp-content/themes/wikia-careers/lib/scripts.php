ca8374577ff64eba32af4486f7ba8ee8f08171b1
Kamil Koterba <kamil.koterba@gmail.com>
2014-07-17 11:39:20 +0200

    Removing empty css block, moving .h4 class to HTML to aviod code repetition

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '25b9bdc80f65a41a276771b912333bdb');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '07e973157e6039c5dbfa99e5e547da30');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- wp-content/themes/wikia-careers/templates/news-feed-section.php
+++ wp-content/themes/wikia-careers/templates/news-feed-section.php
@@ -31,7 +31,7 @@ if( !empty( $posts_array ) ):
 				?>
 				<div class="col-xxs-4 col-xs-4 col-sm-3 col-md-6 col-lg-6 news-item-container">
 					<a href="<?php echo $post_link ?>"><img src="<?php echo $thumb_link ?>" alt="News 2" />
-					<h3><?php echo $post_title ?></h3></a>
+					<h3 class="h4"><?php echo $post_title ?></h3></a>
 					<span class="post-date">Opublikowano: <span class="date"><?php echo $post_date ?></span></span>
 				</div>
 			<?php endforeach; ?>
