36d463e059e4186c5ac9d40935d085d0f1c4c08f
Kamil Koterba <kamil.koterba@gmail.com>
2014-07-18 16:38:36 +0200

    Adjusting titles margins to vertical grid

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6cce361fd5e11ee1078ed3fea718db50');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '59c57e9c1305a622d50f6dd36fc16a4d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
