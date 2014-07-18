7ca135d997c8fb2f22bea42852d0e8ff138c39c4
Kamil Koterba <kamil.koterba@gmail.com>
2014-07-18 16:15:08 +0200

    Minor fix doubled line, lighter dim on banner

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cba61581d896da6b4e31878ef1303d9b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6cce361fd5e11ee1078ed3fea718db50');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
