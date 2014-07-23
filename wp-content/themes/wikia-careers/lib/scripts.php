fbbc6ed6340f6ab49def405cef0740943ac3d012
Kamil Koterba <kamil.koterba@gmail.com>
2014-07-23 16:44:59 +0200

    Adding TinyMCE support for right floated comment for process template

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '70293179523077385e7569d2d234f23c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '523cd04794b2bb62961bfb9f48533f42');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- wp-content/themes/wikia-careers/lib/tinymce-custom-features.php
+++ wp-content/themes/wikia-careers/lib/tinymce-custom-features.php
@@ -20,6 +20,8 @@ function tcf_tiny_mce_before_init($settings) {
 
 	$style_formats = array(
 		array('title' => 'Process h2', 'block' => 'h2', 'classes' => 'process-title'),
+		array('title' => 'Right float comment', 'block' => 'p', 'classes' => 'right-float-comment gutter-left'),
+		array('title' => 'Right float comment last', 'block' => 'p', 'classes' => 'right-float-comment right-float-comment-last gutter-left'),
 		array('title' => 'Process paragraph', 'block' => 'p', 'classes' => 'process-paragraph'),
 		array('title' => 'Process last paragraph', 'block' => 'p', 'classes' => 'process-paragraph-last'),
 	);
