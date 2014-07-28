6b3f27c4ef1614a7ba655cfa2662aef8305f5fa1
Kamil Koterba <kamil.koterba@gmail.com>
2014-07-28 16:37:41 +0200

    Changing dim on banners

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1038dc65a876a393c029c19ff83379c1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6cd61aa8c072ed8d1d2ef24267a3196c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
