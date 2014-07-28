6042187540fae2e90965c40e566128fce46a8a22
Kamil Koterba <kamil.koterba@gmail.com>
2014-07-28 16:09:40 +0200

    Refactoring navigation to be able to add wikia.com button from wordpress panel

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'aa54e0bd397241c8a6841296dfdad84e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1038dc65a876a393c029c19ff83379c1');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
