bc6d5099c5203327624ba1c7bc41f5eea77a4c1f
Kamil Koterba <kamil.koterba@gmail.com>
2014-07-28 18:19:50 +0200

    Making share buttons round

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6cd61aa8c072ed8d1d2ef24267a3196c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd8f53fde953e8c8fa974442ea6be08e8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
