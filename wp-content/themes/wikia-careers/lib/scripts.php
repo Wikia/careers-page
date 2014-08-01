0b5e73c95f5ededbff4416ed35ebf68e2c641b48
Kamil Koterba <kamil@wikia-inc.com>
2014-08-01 14:35:41 +0200

    Adding default header background

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b1cfb377122ccb15f1e02fe03bc7d947');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '52e7fb2b04814611448618d26a3a5700');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
