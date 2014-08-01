292f6f41036930d1dc44430baf9e05ef745f40f0
Kamil Koterba <kamil@wikia-inc.com>
2014-08-01 17:55:55 +0200

    Quick fix for share buttons layout

--- wp-content/themes/wikia-careers/lib/scripts.php
+++ wp-content/themes/wikia-careers/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '52e7fb2b04814611448618d26a3a5700');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0ffc1cc8e0976dc1565dec99fd3993c6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
